83953a0b9b6b51ca5ada13e7a5e1397a317819bb
Ronald Mize Jr <rwthojr5@gmail.com>
2025-04-19 00:25:36 -0500

    init and nativewind setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
+import './styles/global.css';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to voxify!</Text>
+    <View className='flex-1 items-center justify-center bg-white'>
+      <Text className='text-4xl font-bold'>Welcome to Voxify!</Text>
       <StatusBar style='auto' />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
