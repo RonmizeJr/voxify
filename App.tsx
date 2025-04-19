147b28cb7f46d27f082b0ce17de0e305dc3e8ab7
Ronald Mize Jr <rwthojr5@gmail.com>
2025-04-19 09:00:37 -0500

    init

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
-import './styles/global.css';
+import './global.css';
 
 export default function App() {
   return (
-    <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='text-4xl font-bold'>Welcome to Voxify!</Text>
-      <StatusBar style='auto' />
+    <View className="flex-1 items-center justify-center bg-slate-950">
+      <Text className="text-2xl font-bold text-red-500">Welcome to Voxify!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './App.tsx'],
+  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+
   presets: [require('nativewind/preset')],
   theme: {
     extend: {},
